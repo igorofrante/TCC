7d79e916a77d04fb3ecd2f00555ef447754f0a7c
igorofrante <igorofrante@gmail.com>
2022-07-26 20:23:49 -0300

    v0.1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    mit_bal = models.IntegerField() 
+    sex = models.PositiveSmallIntegerField()
+    education = models.PositiveSmallIntegerField()
+    marriage = models.PositiveSmallIntegerField()
+    age = models.PositiveSmallIntegerField()
+    pay_1 = models.SmallIntegerField()
+    pay_2 = models.SmallIntegerField()
+    pay_3 = models.SmallIntegerField()
+    pay_4 = models.SmallIntegerField()
+    pay_5 = models.SmallIntegerField()
+    pay_6 = models.SmallIntegerField()
+    bill_amt_1 = models.IntegerField() 
+    bill_amt_2 = models.IntegerField() 
+    bill_amt_3 = models.IntegerField() 
+    bill_amt_4 = models.IntegerField() 
+    bill_amt_5 = models.IntegerField() 
+    bill_amt_6 = models.IntegerField()
+    pay_amt_1 = models.IntegerField()
+    pay_amt_2 = models.IntegerField()
+    pay_amt_3 = models.IntegerField()
+    pay_amt_4 = models.IntegerField()
+    pay_amt_5 = models.IntegerField()
+    pay_amt_6 = models.IntegerField()
+    payment = models.BinaryField()
+    class Meta:
+        db_table = "clientes"  
