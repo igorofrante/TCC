63df6c4395de5759a9191073b57bcba2cb8340c8
igorofrante <igorofrante@gmail.com>
2022-08-03 20:26:51 -0300

    v0.75

--- TCC/urls.py
+++ TCC/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index),
+    path('dashboard/',views.dashboardIndex),
+    path('client/',views.clienteIndex)
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from app.forms import * 
+from app.models import *
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def dashboardIndex(request):
+    return render(request,'dashboard.html')
+
+def clienteIndex(request):
+    return render(request,'client.html')
+
